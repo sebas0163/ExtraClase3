22486e76782857f98f91081ea3e84c5143b75ca3
Sebas <sebas0163@hotmail.com>
2019-10-04 11:55:56 -0600

    Base para mostrar en pantalla

--- app/src/main/java/com/example/ordenamiento/Main3Activity.java
+++ app/src/main/java/com/example/ordenamiento/Main3Activity.java
@@ -6,9 +6,51 @@ import android.os.Bundle;
 
 public class Main3Activity extends AppCompatActivity {
 
+    private int swaps=0;
+    private int comp=0;
+    private DoubleEndedLinkedList<Integer> lista = MainActivity.lista;
+    private int[] array;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        array = convertirArray();
+        bubbleSort(array);
+    }
+    public int[] bubbleSort(int[] numArray) {
+
+        int n = numArray.length;
+        int temp = 0;
+        int itera=0;
+//    int swaps=0;
+//    int comp=0;
+
+        for (int i = 0; i < n; i++) {
+            itera++;
+            comp++;
+            for (int j = 1; j < (n - i); j++) {
+                comp++;
+                if (numArray[j - 1] > numArray[j]) {
+                    swaps++;
+                    temp = numArray[j - 1];
+                    numArray[j - 1] = numArray[j];
+                    numArray[j] = temp;
+                }
+
+            }
+            mostrarIteraciones(numArray,itera,swaps,comp);
+        }
+        return numArray;
+    }
+    private void mostrarIteraciones(int[] array,int iteracion,int swaps,int compara){
+        return;
+    }
+    private int[] convertirArray(){
+        int[] listaAux = new int[lista.len()];
+        for (int i =0; i<lista.len(); i++){
+            listaAux[i]= lista.getInfo(i);
+        }
+        return listaAux;
     }
 }
--- app/src/main/java/com/example/ordenamiento/Main4Activity.java
+++ app/src/main/java/com/example/ordenamiento/Main4Activity.java
@@ -5,10 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main4Activity extends AppCompatActivity {
+    private int swaps=0;
+    private int comp=0;
+    private DoubleEndedLinkedList<Integer> lista = MainActivity.lista;
+    private int[] array;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        array = convertirArray();
+        insert(array);
+    }
+    /**
+     *tomado de
+     * https://www.geeksforgeeks.org/insertion-sort/
+     */
+    public int[] insert(int[] numArray){
+        int n = numArray.length;
+        int itera = 0;
+        for (int i = 1; i < n; ++i) {
+            itera++;
+            comp++;
+            int key = numArray[i];
+            int j = i - 1;
+            while (j >= 0 && numArray[j] > key) {
+                comp++;
+                numArray[j + 1] = numArray[j];
+                swaps++;
+                j = j - 1;
+            }
+            numArray[j + 1] = key;
+            mostrarIteraciones(numArray,itera,swaps,comp);
+        }
+        comp--;
+        return numArray;
+    }
+    private void mostrarIteraciones(int[] array,int iteracion,int swaps,int compara){
+        return;
+    }
+    private int[] convertirArray(){
+        int[] listaAux = new int[lista.len()];
+        for (int i =0; i<lista.len(); i++){
+            listaAux[i]= lista.getInfo(i);
+        }
+        return listaAux;
     }
 }
